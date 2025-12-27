a97694d5da65b85b378fb73bed66d7f3ca123245
Kalleanka999 <adam.bjork08@icloud.com>
2025-12-27 23:06:06 +0100

    Delete App/verkstad/admin/page.tsx

--- App/verkstad/admin/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Admin() {
-  return (
-    <main className="container">
-      <h2>Adminöversikt</h2>
-      <p>Här visas offerter och bokningar (MVP).</p>
-    </main>
-  );
-}
